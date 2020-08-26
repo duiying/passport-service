7552d373b127efce1e945ca8acbae22fe1e1939c
wangyaxian <wangyaxian@higohappy.com>
2020-08-26 12:09:15 +0800

    feat: init

---
+++ config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use HyperfPlus\Route\Route;
+
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+use Hyperf\HttpServer\Router\Router;
+
+Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'HyperfPlus\Controller\IndexController@handle');
